2c0d21a01eb988c0077493afba6f3f10f18071ab
Qigang Xu <xuqigang10@163.com>
2017-07-29 16:38:58 +0800

    添加FontAwesome在UIimageView中的演示

--- FontAwesome/ViewController.m
+++ FontAwesome/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
-
+#import "NSString+FontAwesome.h"
+#import "UIFont+FontAwesome.h"
+#import "UIImage+FontAwesome.h"
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 
 @end
 
@@ -17,9 +22,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self fontWesomeLabel];
+    [self fontWesomeImageView];
+}
+//UILabel演示
+- (void) fontWesomeLabel
+{
+    NSString *weiboImageStr = [NSString fontAwesomeIconStringForIconIdentifier:@"fa-weibo"];
+    UIFont * font = [UIFont fontAwesomeFontOfSize:18];
+    NSMutableAttributedString *weiboImageAttrStr = [[NSMutableAttributedString alloc] initWithString:weiboImageStr attributes:@{NSFontAttributeName : font}];
+    NSString *weiboStr = @" 新浪微博";
+    NSMutableAttributedString *weiboAttrStr = [[NSMutableAttributedString alloc] initWithString:weiboStr attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:14], NSForegroundColorAttributeName: [UIColor blackColor]}];
+    [weiboImageAttrStr appendAttributedString:weiboAttrStr];
+    self.label1.attributedText = weiboImageAttrStr;
 }
 
-
+//UIImageView演示
+- (void) fontWesomeImageView
+{
+    UIImage * image = [UIImage imageWithIcon:@"fa-plug" backgroundColor:[UIColor clearColor] iconColor:[UIColor blackColor] andSize:CGSizeMake(15, 15)];
+    self.imageView.image = image;
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
